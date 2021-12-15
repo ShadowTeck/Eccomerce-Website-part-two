567d3ca364536629a97be0456544d9b0dbca19cf
Shadow_Teck <32403338+ShadowTeck@users.noreply.github.com>
2021-12-15 15:05:03 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+require("express-async-errors")
+
+const express = require('express')
+const app = express()
+const fileUpload = require('express-fileupload')
+
+const connectDB = require('./DB/connect')
+const productRouter = require("./routes/productsRouter")
+
+//cloudinary
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+    cloud_name: process.env.cloud_name
+})
+
+const notFoundError = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFound = require("./middleware/not-found")
+
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(fileUpload({useTempFiles: true}))
+app.use(express.static('./public'))
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.get('/', (req, res) => {
+    res.send('<h1>File Upload Starter</h1>')
+})
+app.use('/api/v1/products', productRouter)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const start = async() => {
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`listening @ ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
